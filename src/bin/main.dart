11f628dc5902996ddac50d1f14de4326eea8600b
David Langheiter <david@langheiter.com>
2019-12-12 22:17:01 +0100

    Added different config loading on different enviorments

--- src/bin/main.dart
+++ src/bin/main.dart
@@ -1,12 +1,38 @@
+import 'dart:math';
+
+import 'package:args/args.dart';
+import 'package:src/config/application_config.dart';
 import 'package:src/src.dart';
 
-Future main() async {
-  final app = Application<SrcChannel>()
-      ..options.configurationFilePath = "config.yaml"
-      ..options.port = 8888;
+final confs = {
+  "deployment": "config.yaml",
+  "development": "config.dev.yaml",
+  "test": "config.src.yaml",
+};
 
-  final count = Platform.numberOfProcessors ~/ 2;
-  await app.start(numberOfInstances: count > 0 ? count : 1);
+Future main(List<String> args) async {
+  final parser = ArgParser();
+  parser.addOption('mode',
+      abbr: 'm',
+      defaultsTo: 'deployment',
+      allowed: ['deployment', 'development', 'test']);
+  final results = parser.parse(args);
+
+  final app = Application<SrcChannel>();
+
+  app.options.configurationFilePath = confs[results['mode']];
+
+  final conf = ApplicationConfiguration(app.options.configurationFilePath);
+
+  var numProcessors = Platform.numberOfProcessors;
+  if(conf.server.processors > 0) {
+    numProcessors = min(numProcessors, conf.server.processors);
+  } else if(conf.server.processors < 0) {
+    numProcessors = min(numProcessors, numProcessors - conf.server.processors);
+  }
+  app.options.port = conf.server.port;
+
+  await app.start(numberOfInstances: max(1, numProcessors));
 
   print("Application started on port: ${app.options.port}.");
   print("Use Ctrl-C (SIGINT) to stop running the application.");
